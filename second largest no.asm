a923c5e587c57e0a74d2a3af7003d85f58d08b02
Aneena Antony <73019565+Aneenaanu321@users.noreply.github.com>
2022-04-22 02:10:16 +0530

    Create second largest no.asm

---
+++ second largest no.asm
@@ -0,0 +1,66 @@
+.model small
+
+.stack
+    print macro p
+    lea dx,p 
+    mov ah,09h
+    int 21h
+    endm
+
+    printvar macro p
+    mov dl,p 
+    mov ah,02h
+    int 21h
+    endm
+
+.data
+    msg1 db 10,13,"enter size of array:$"
+    msg2 db 10,13,"enter elements into array(0-9): $"
+    msg3 db 10,13,"second largest number is: $"
+    num db 1 dup(?)
+    arr db 10 dup(?)
+
+.code
+    mov ax,@data
+    mov ds,ax
+    print msg1
+    mov ah,01h
+    int 21h
+    sub al,30h
+    mov num,al
+    mov cl,num
+    mov ch,00h
+    print msg2
+    lea si,arr
+    input:  mov ah,01h
+            int 21h
+            sub al,30h
+            mov [si],al
+            inc si
+            loop input
+        
+    mov dl,num
+    dec dl
+    mov dh,00h
+    mov bx,dx
+    l1: mov cx,dx
+        lea si,arr
+    l2: mov al,[si]
+        cmp al,[si+1]
+        jnc l3
+        xchg al,[si+1]
+        mov [si],al
+    l3: inc si
+        loop l2
+        dec bx
+        jnz l1
+
+    print msg3
+    lea si,arr
+    mov al,[si+1]
+    add al,30h
+    printvar al
+
+    mov ah,4ch
+    int 21h
+    end
